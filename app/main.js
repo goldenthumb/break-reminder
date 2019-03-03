c95d226300dd6de4b33936ac9e7e089b362b64bc
yhchoi <cyh8914@gmail.com>
2019-03-03 23:41:13 +0900

    add tray

--- app/main.js
+++ app/main.js
@@ -1,16 +1,36 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Tray } = require('electron');
+const path = require('path');
 
 let mainWindow = null;
+let tray = null;
 
 if (process.env.NODE_ENV === 'development') {
   require('electron-debug')();
 }
 
 const createWindow = () => {
+  tray = new Tray(path.resolve(__dirname, '../resources/tray.png'));
+
+  tray.on('click', () => {
+    if (mainWindow.isVisible()) {
+      mainWindow.hide();
+    } else {
+      mainWindow.show();
+    }
+  });
+
+  const bounds = tray.getBounds();
+
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    show: false
+    width: 400,
+    height: 400,
+    x: Math.round(bounds.x + (bounds.width / 2) - 200),
+    y: bounds.y - 400 - 10,
+    acceptFirstMouse: true,
+    show: false,
+    resizable: false,
+    movable: false,
+    frame: false,
   });
 
   mainWindow.loadURL(`file://${__dirname}/index.html`);
@@ -18,10 +38,6 @@ const createWindow = () => {
   if (process.env.NODE_ENV === 'development') {
     mainWindow.webContents.openDevTools();
   }
-
-  mainWindow.once('ready-to-show', () => {
-    mainWindow.show();
-  });
 
   mainWindow.on('closed', () => {
     app.quit();
