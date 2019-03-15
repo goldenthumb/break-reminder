3da7be14b5384c15074da538d11607bedf808cc2
yhchoi <cyh8914@gmail.com>
2019-03-15 23:19:57 +0900

    update tray icon

--- app/main.js
+++ app/main.js
@@ -21,7 +21,7 @@ const store = new Store({
 });
 
 const createWindow = () => {
-  tray = new Tray(path.resolve(__dirname, '../resources/tray.png'));
+  tray = new Tray(path.resolve(__dirname, '../resources/icon/tray.png'));
 
   tray.on('click', () => {
     if (mainWindow.isVisible()) {
