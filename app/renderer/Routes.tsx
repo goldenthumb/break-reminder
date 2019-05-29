d13f577c28375e5769667176383e845c25e91c53
yhchoi <cyh8914@gmail.com>
2019-05-30 00:21:36 +0900

    set typescript

--- app/renderer/Routes.tsx
+++ app/renderer/Routes.tsx
@@ -3,15 +3,17 @@ import { parse } from 'query-string';
 import { RouteComponentProps } from 'react-router-dom';
 
 import Main from './windows/Main';
+import Break from './windows/Break';
+import Overlay from './windows/Overlay';
 
 const Routes = ({ location: { search } }: RouteComponentProps) => {
-  if (search) {
-    const { window } = parse(search);
+  if (!search) return null;
 
-    if (window === 'main') return <Main />;
-    // if (window === 'break') return <Break />;
-    // if (window === 'overlay') return <Overlay />;
-  }
+  const { window } = parse(search);
+
+  if (window === 'main') return <Main />;
+  if (window === 'break') return <Break />;
+  if (window === 'overlay') return <Overlay />;
 
   return null;
 };
--- app/renderer/windows/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Main } from './Main';
-export { default as Break } from './Break';
-export { default as Overlay } from './Overlay';
