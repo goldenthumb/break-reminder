e76847b019187c09ea3759cc65ccf29312e7271c
yhchoi <cyh8914@gmail.com>
2019-06-03 22:18:34 +0900

    remove complied js

--- app/renderer/components/Body/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = __importDefault(require("react"));
-const css = require('./Body.scss');
-const Body = ({ children }) => (react_1.default.createElement("div", { className: css['body'] }, children));
-exports.default = Body;
--- app/renderer/components/Button/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = __importDefault(require("react"));
-const css = require('./Button.scss');
-const Button = ({ theme, action, children }) => (react_1.default.createElement("button", { type: 'button', className: css[theme], onClick: action }, children));
-exports.default = Button;
