3b05850df41c87e934330f8330829dcb4a2239f0
yhchoi <yhchoi@rsupport.com>
2019-07-23 10:45:31 +0900

    add disabled attribute to button

--- app/renderer/components/Button/index.tsx
+++ app/renderer/components/Button/index.tsx
@@ -4,14 +4,16 @@ const css = require('./Button.scss');
 interface ButtonProps {
   theme: string;
   action: () => void;
+  disabled?: boolean;
   children: React.ReactNode;
 }
 
-export default function Button({ theme, action, children }: ButtonProps) {
+export default function Button({ theme, action, children, disabled }: ButtonProps) {
   return (
     <button
       type='button'
       className={css[theme]}
+      disabled={disabled}
       onClick={action}
     >
       {children}
